5bc7595042463329565b24718711c474470fab17
Akash Vikram Shroff <63399889+akashvshroff@users.noreply.github.com>
2020-07-12 23:06:02 +0530

    Fixed the sidebar bug.

--- users/views.py
+++ users/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
+from Blog.models import Post
+import random
 # Create your views here.
 
 
